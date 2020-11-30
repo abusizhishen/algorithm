fa2397813df74c57c98ede635921f019a88479b9
abusizhishen <abusizhishen@gmail.com>
2020-12-01 00:18:55 +0800

    反转

--- array/189.rotate-array/reverse.go
+++ array/189.rotate-array/reverse.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func main() {
+	var nums =[]int{1,2,3,4,5,6,7}
+	var k = 3
+	var l = len(nums)-1
+	k %= len(nums)
+
+	reverse(nums,0,l)
+	reverse(nums,0,k-1)
+	reverse(nums,k,l)
+	fmt.Println(nums)
+}
+
+func reverse(nums []int, start,end int)  {
+	for start<=end{
+		nums[start],nums[end] = nums[end],nums[start]
+		start++
+		end--
+	}
+}
